360ef2e478de204bcd517c0db7f306bb2c0f902e
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-08-14 00:41:09 -0700

    Fix segfault on new trimesh

--- src/shape/convex_hull.rs
+++ src/shape/convex_hull.rs
@@ -10,6 +10,7 @@
 extern crate nalgebra as na;
 
 use crate::nc;
+use std::sync::Arc;
 
 pub fn convex_hull_new(ptr_points: *const [f64; nc::math::DIM],
                        npoints: usize) -> *mut nc::shape::ShapeHandle<f64> {
@@ -17,9 +18,17 @@ pub fn convex_hull_new(ptr_points: *const [f64; nc::math::DIM],
     let points = unsafe { std::slice::from_raw_parts(ptr_points, npoints) };
     let points: Vec<_> = points.iter().map(|x| nc::math::Point::<f64>::from_slice(x)).collect();
 
-    let trimesh = nc::shape::ConvexHull::try_from_points(&points).unwrap();
-    let handle = nc::shape::ShapeHandle::new(trimesh);
-    Box::into_raw(Box::new(handle))
+    let maybe_trimesh = nc::shape::ConvexHull::try_from_points(&points);
+    match maybe_trimesh {
+        Some(trimesh) => {
+            let handle = nc::shape::ShapeHandle::new(trimesh);
+            Box::into_raw(Box::new(handle))
+        }
+        None => {
+            let trimesh = nc::shape::TriMesh::new(Vec::new(), Vec::new(), Option::None);
+            Box::into_raw(Box::new(nc::shape::ShapeHandle::new(trimesh)))
+        }
+    }
 }
 
 pub fn convex_hull_num_points(shape: Option<&nc::shape::ShapeHandle<f64>>) -> usize {
